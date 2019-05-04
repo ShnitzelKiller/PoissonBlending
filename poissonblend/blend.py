62c11222ad1934d84262218b588a657c2a561fec
James Noeckel <jamesn8@uw.edu>
2019-05-03 18:10:41 -0700

    ayylmao

--- poissonblend/blend.py
+++ poissonblend/blend.py
@@ -108,7 +108,6 @@ def blend(image, mask, guide=None, offset=(0,0), threshold=0.5, boundary_guide =
         img2[p] = xs[i]
     mask = np.expand_dims(mask,2)
     if resized:
-        print('mask.shape:',mask.shape,'image.shape:',image.shape)
         image_copy = image_orig.astype(np.float)
         image_copy[offset[0]:ends[0],offset[1]:ends[1]] = (image * (1-mask) + img2 * mask)[inv_offsets[0]:inv_ends[0],inv_offsets[1]:inv_ends[1]]
         return image_copy
